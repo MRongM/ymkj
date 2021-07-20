9cbba62e41a25a98341ce6e75c88380b2a529d8d
MRongM <idskof@sina.cn>
2021-07-20 11:57:05 +0800

    init

---
+++ server.js
@@ -0,0 +1,14 @@
+var express = require("express");
+var ejs = require("ejs");
+var app = express();
+
+app.set('view engine','ejs');
+app.use(express.static(__dirname + '/public'));
+
+data = {"title":"YMKJ"}
+
+app.get("/",function(req, res){
+	res.render("index", data);	
+})
+
+app.listen(3000);
